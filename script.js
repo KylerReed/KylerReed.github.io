12a3ffc3523cb5da6061b7e633015c05e27bfc9f
KylerReed <113257958+KylerReed@users.noreply.github.com>
2025-12-01 19:24:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const button = document.getElementById("theme-toggle");
-const body = document.body;
-
-button.addEventListener("click", () => {
-  body.classList.toggle("light");
-  body.classList.toggle("dark");
-});
 
